e7328a3985282b51f708f4b165571818593b795c
Kavanih <116088413+Kavanih@users.noreply.github.com>
2023-06-12 10:27:31 +0100

    home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,30 +1,37 @@
 const Home = () => {
   return (
-    <div className="w-[90%] sm:w-3/5 mx-auto bg-color2 text-color1">
-      <div className="ti">Low House Edge</div>
-      <div className="content mb-4">
-        At Vviper, we take pride in offering an unparalleled gambling experience
-        with a significant advantage in your favor. Our commitment to providing
-        exceptional odds is evident through our exceptionally low house edge,
-        setting us apart from traditional casinos and other platforms.
+    <div className="w-4/5 mx-auto bg-color2 text-color1 p-6 rounded-md grid grid-cols-2">
+      <div className="">
+        <div className="ti">Low House Edge</div>
+        <div className="content mb-4">
+          At Vviper, we take pride in offering an unparalleled gambling
+          experience with a significant advantage in your favor. Our commitment
+          to providing exceptional odds is evident through our exceptionally low
+          house edge, setting us apart from traditional casinos and other
+          platforms.
+        </div>
       </div>
-      <div className="ti">No KYC</div>
-      <div className="content mb-4">
-        At Vviper, we revolutionize the gambling experience by eliminating the
-        burdensome process of user registration and Know Your Customer (KYC)
-        requirements. We firmly believe in providing a seamless and hassle-free
-        gaming environment, allowing you to focus solely on the thrill of
-        placing bets and winning. Gone are the days of submitting personal
-        information, providing identification documents, and waiting for
-        verification processes.
+      <div className="">
+        <div className="ti">No KYC</div>
+        <div className="content mb-4">
+          At Vviper, we revolutionize the gambling experience by eliminating the
+          burdensome process of user registration and Know Your Customer (KYC)
+          requirements. We firmly believe in providing a seamless and
+          hassle-free gaming environment, allowing you to focus solely on the
+          thrill of placing bets and winning. Gone are the days of submitting
+          personal information, providing identification documents, and waiting
+          for verification processes.
+        </div>
       </div>
-      <div className="ti">Provably Fair Betting</div>
-      <div className="content mb-4">
-        At Vviper, we prioritize fairness and transparency in every aspect of
-        our betting system. To ensure the integrity of our games, we have
-        implemented a cutting-edge technology called Chainlink VRF (Verifiable
-        Random Function) combined with transparent odds displayed publicly in
-        our game smart contracts.
+      <div className="col-span-2">
+        <div className="ti">Provably Fair Betting</div>
+        <div className="content mb-4">
+          At Vviper, we prioritize fairness and transparency in every aspect of
+          our betting system. To ensure the integrity of our games, we have
+          implemented a cutting-edge technology called Chainlink VRF (Verifiable
+          Random Function) combined with transparent odds displayed publicly in
+          our game smart contracts.
+        </div>
       </div>
     </div>
   );
