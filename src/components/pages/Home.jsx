302d8f98cca687578f79264c6c254bb0bf6d96c8
Kavanih <116088413+Kavanih@users.noreply.github.com>
2023-06-12 13:17:39 +0100

    icon

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,6 @@
+import { GiTakeMyMoney, GiRapidshareArrow, GiCardBurn } from "react-icons/gi";
+import { MdOutlineVerified } from "react-icons/md";
+
 const Home = () => {
   return (
     <div className="w-full mx-auto bg-color2 text-color1 p-6 rounded-md grid grid-cols-2 gap-4">
@@ -5,7 +8,9 @@ const Home = () => {
         Welcome to Viper Casino
       </div>
       <div className="">
-        <div className="ti">Play & EARN FOR FREE</div>
+        <div className="ti">
+          <GiTakeMyMoney /> Play 2 EARN FOR FREE
+        </div>
         <div className="content mb-4">
           At Venom Viper Casino, you have the opportunity to play using $VViper,
           the Viper Casino native token on Venom Blockchain that can be claimed
@@ -15,7 +20,9 @@ const Home = () => {
         </div>
       </div>
       <div className="">
-        <div className="ti">Low House Edge</div>
+        <div className="ti">
+          <GiRapidshareArrow /> Low House Edge
+        </div>
         <div className="content mb-4">
           At Vviper, we take pride in offering an unparalleled gambling
           experience with a significant advantage in your favor. Our commitment
@@ -25,7 +32,9 @@ const Home = () => {
         </div>
       </div>
       <div className="">
-        <div className="ti">No KYC</div>
+        <div className="ti">
+          <MdOutlineVerified /> No KYC
+        </div>
         <div className="content mb-4">
           At Vviper, we revolutionize the gambling experience by eliminating the
           burdensome process of user registration and Know Your Customer (KYC)
@@ -35,7 +44,9 @@ const Home = () => {
         </div>
       </div>
       <div className="">
-        <div className="ti">Provably Fair Betting</div>
+        <div className="ti">
+          <GiCardBurn /> Provably Fair Betting
+        </div>
         <div className="content mb-4">
           At Vviper, we prioritize fairness and transparency in every aspect of
           our betting system. To ensure the integrity of our games, we have
